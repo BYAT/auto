7b9296841038284afa16160ecf9b17f06ad948bc
Danya <mudifea.danya@gmail.com>
2020-09-01 17:43:03 +0300

    applying myip to all

--- auto.sh
+++ auto.sh
@@ -1,20 +1,20 @@
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket install-plugin github-pullrequest
 java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job department-service < department-service.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job office-service < office-service.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job person-service < person-service.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job project-assessment-site < project-assessment-site.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job role-service < role-service.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job Packer < Packer.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job elf < elf.xml
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket create-job pro-graf < pro-graf.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job office-service < office-service.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job person-service < person-service.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job project-assessment-site < project-assessment-site.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job role-service < role-service.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job Packer < Packer.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job elf < elf.xml
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket create-job pro-graf < pro-graf.xml
 
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build department-service
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build office-service
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build person-service
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build project-assessment-site
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build role-service
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build Packer
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build elf
-java -jar jenkins-cli.jar -s http://${JENKINS_SERVICE_HOST}/jenkins/ -webSocket build pro-graf
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build department-service
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build office-service
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build person-service
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build project-assessment-site
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build role-service
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build Packer
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build elf
+java -jar jenkins-cli.jar -s http://${myip}/jenkins/ -webSocket build pro-graf
 
